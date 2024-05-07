e87da2b2a55f2efd2d1766d8c0869d51d9969c33
lmatsuii <leonardomatsuii@gmail.com>
2024-05-06 22:39:23 -0300

    Criado projeto .net com framework MVC

---
+++ codigo-fonte/reserva_sala/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reserva_sala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
