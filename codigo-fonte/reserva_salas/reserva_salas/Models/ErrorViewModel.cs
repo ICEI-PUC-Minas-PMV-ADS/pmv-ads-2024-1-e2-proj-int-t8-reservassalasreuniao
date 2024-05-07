348234e1e5d298e3f0605d2e8cbd096e09ee7e5b
lmatsuii <leonardomatsuii@gmail.com>
2024-05-06 23:18:59 -0300

    Removido projeto antigo

--- codigo-fonte/reserva_salas/reserva_salas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace reserva_salas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
