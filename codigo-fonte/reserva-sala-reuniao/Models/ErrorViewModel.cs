aafe4e0681052b09bc3111f9fe240469b0ee390e
lmatsuii <leonardomatsuii@gmail.com>
2024-05-06 22:57:26 -0300

    Teste

---
+++ codigo-fonte/reserva-sala-reuniao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reserva_sala_reuniao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
