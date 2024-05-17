344811a2a7ba4adb288c9ec34cf5e041cc251a10
Robert Magnani <1501484@sga.pucminas.br>
2024-05-17 12:00:09 -0600

    coloquei as hierarquias de usuarios e restricoes (Admin, User). implante o logout e removi pastas e arquivos para o funcionamento correto das funcoes implantadas.

--- codigo-fonte/reserva-sala-reuniao/Controllers/HomeController.cs
+++ codigo-fonte/reserva-sala-reuniao/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace reserva_sala_reuniao.Controllers
 {
     public class HomeController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
