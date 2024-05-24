a285410442afcea436f19227b4355bd6b498c6b0
Robert Magnani <1501484@sga.pucminas.br>
2024-05-24 01:22:35 -0600

    Nao aprovar esse por favor.

--- codigo-fonte/reserva-sala-reuniao/Controllers/UsuariosController.cs
+++ codigo-fonte/reserva-sala-reuniao/Controllers/UsuariosController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< HEAD
-=======
 using System.Security.Cryptography;
->>>>>>> develop
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
