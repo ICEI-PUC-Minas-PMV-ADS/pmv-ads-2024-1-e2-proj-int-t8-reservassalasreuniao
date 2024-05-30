632bfcf564f38ee954f93519ffc555fabbe9cfdd
Thais Bertini <thaisbertini.ux@hotmail.com>
2024-05-30 17:42:25 -0300

    Connection string novo banco de dados sql server Azure

--- codigo-fonte/reserva-sala-reuniao/Models/AppDbContext.cs
+++ codigo-fonte/reserva-sala-reuniao/Models/AppDbContext.cs
@@ -19,30 +19,30 @@ namespace reserva_sala_reuniao.Models
 
         public DbSet<Cargo> Cargos { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.Admin> Admin { get; set; }
+        public DbSet<reserva_sala_reuniao.Models.Admin> Admin { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.Localizacao> Localizacao { get; set; }
+        public DbSet<reserva_sala_reuniao.Models.Localizacao> Localizacao { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.Reserva> Reserva { get; set; }
+        public DbSet<reserva_sala_reuniao.Models.Reserva> Reserva { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.Sala> Sala { get; set; }
+        public DbSet<reserva_sala_reuniao.Models.Sala> Sala { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.Setor> Setor { get; set; }
+        public DbSet<reserva_sala_reuniao.Models.Setor> Setor { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.Usuario> Usuario { get; set; }
+        public DbSet<reserva_sala_reuniao.Models.Usuario> Usuario { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.Relatorio> Relatorio { get; set; }
+        public DbSet<reserva_sala_reuniao.Models.Relatorio> Relatorio { get; set; }
 
-public DbSet<reserva_sala_reuniao.Models.RelatorioTipo> RelatorioTipo { get; set; }
-        /*public DbSet<Setor> Setores { get; set; }
-        public DbSet<Admin> Admins { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Localizacao> Localizacoes { get; set; }
-        public DbSet<Sala> Salas { get; set; }
-        public DbSet<Reserva> Reservas { get; set; }
-        public DbSet<RelatorioTipo> RelatorioTipos { get; set; }
-        public DbSet<Relatorio> Relatorios { get; set; }*/
+        public DbSet<reserva_sala_reuniao.Models.RelatorioTipo> RelatorioTipo { get; set; }
+                /*public DbSet<Setor> Setores { get; set; }
+                public DbSet<Admin> Admins { get; set; }
+                public DbSet<Usuario> Usuarios { get; set; }
+                public DbSet<Localizacao> Localizacoes { get; set; }
+                public DbSet<Sala> Salas { get; set; }
+                public DbSet<Reserva> Reservas { get; set; }
+                public DbSet<RelatorioTipo> RelatorioTipos { get; set; }
+                public DbSet<Relatorio> Relatorios { get; set; }*/
 
-        }
+                }
 
- }
+    }
